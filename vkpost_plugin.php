8117ce52a612e6f96506f4c8dc6271dfb14e8ba5
1px <0test@mail.ru>
2016-10-06 23:45:59 +0300

    Create vkpost_plugin.php

---
+++ vkpost_plugin.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * vkpost_old plugin
+ * @version 0.2.0 (2015-12-17)
+ * 
+ * @for MODx Evolution 1.0.x
+ * 
+ * @desc posting the document fields on the wall of the vk.com
+ * 
+ * @link https://vk.com/remote_adm
+ * 
+ * @author 1px(www.1px.su), Bocharsky Victor, https://github.com/Vastly
+ *
+ * @copyright 2015
+ 
+https://vk.com/editapp?id=4995526&section=options
+ */
+defined('IN_MANAGER_MODE') or die();
+global $e;
+$e = &$modx->Event;
+	if(!isset($e->params['clubId'])) {
+		show_error(1);
+		return false;
+	}
+	else{
+		$clubId=$e->params['clubId'];
+	}
+	
+	if(!isset($e->params['accessToken'])) {
+		show_error(2);
+		return false;
+	}
+	else{
+		$accessToken=$e->params['accessToken'];
+	}
+
+	if(!isset($e->params['tvName'])) {
+		show_error(3);
+		return false;
+	}
+	else{
+		$tvName=$e->params['tvName'];
+	}
+	
+	if(!isset($e->params['postTemplate'])) {
+		show_error(4);
+		return false;
+	}
+	else{
+		$postTemplate=$e->params['postTemplate'];
+	}	
+
+function show_error($errNum){
+	$errors=array('','Вы не ввели id группы','Вы не ввели accessToken','Вы не ввели ID TV','Шаблон поста пустой');
+	echo '<div class="error">Ошибка в плагине vkpost: '.$errors[$errNum].'</div>';
+}
+
+$output = '';
+if ($e->name == 'OnDocFormRender') //Здесь код, который выполнится при открытии документа
+{
+	$output .='<!-- VK POST -->'."\n";
+	$output .='<script>'."\n";
+	$output .='$j("head").append(\'<link rel="stylesheet" href="'. $modx->config['site_url'] .'assets/plugins/vkpost_old/widget.css?21">\');' ."\n";//Подключаем стиль css
+	$output .='mytv="#tv'.$tvName.'";'."\n"; //значение ТВ из конфига передаём в переменную для JS.
+	$output .='thisid='.$id.';' ."\n"; //ID текущего документа передаём в переменную для JS
+	//Переделываем внешний вид ТВшки
+	$output .='elems=\'<div class="vk_post_wrapper actionButtons">\';'."\n";
+	$output .='elems = elems + \'<div class="vk_post_count">Постов: \'+$j(mytv).val()+\'</div>\';'."\n";
+	$output .='elems = elems +\'<div class="vk_post_result">&nbsp;</div>\';' . "\n";
+	$output .='elems = elems + \'<div><input class="vk_post_action" type="button" value="Запостить"></div>\';'."\n";
+	$output .='elems = elems + \'</div>\';'."\n";
+	
+	$output .= '$j(mytv).parents("td").append(elems);'."\n";//Добавляем упр. элементы рядом с твшкой
+	$output .= '$j(mytv).hide();'."\n"; //Скрываем оригинальную твшку
+	$output .='$j("head").append(\'<script type="text/javascript" src="'. $modx->config['site_url'] .'assets/plugins/vkpost_old/events.js?2"></scr\'+\'ipt>\');' ."\n";	//Добавляем скрипт с аяком
+	$output .='</script>'."\n";
+	$output .='<!-- END VK POST -->'."\n";
+	
+}
+
+$e->output($output . "\n");
+?>
